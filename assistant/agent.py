876cc8870e1e1eac90c70ebf42c43f1e7e79fde6
JulesDimanche <julesdimanche200479@gmail.com>
2025-12-27 09:14:44 +0530

    added google_search tools to it

--- assistant/agent.py
+++ assistant/agent.py
@@ -1,8 +1,10 @@
 from google.adk.agents import LlmAgent
+from google.adk.tools import google_search
 tech_agent=LlmAgent(
     name='Tech_News_Agent',
     model='gemini-2.5-flash',
     description='An agent that provides the latest updates in technology news.',
     instruction="""You are Technical News Agent, your task is to provide the latest updates in technology news and AI related news. Always respond with the most recent and relevant information available.""",
+    tools=[google_search],
 )
 root_agent=tech_agent
